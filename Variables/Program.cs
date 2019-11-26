2ae34e7dc15800e0f56a79d113c499e09a1f3eae
torrance <torrancehenderson11@gmail.com>
2019-11-26 15:30:52 -0600

    Add project files.

---
+++ Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'a';
+            string firstName = "Torrance";
+            bool isWorking = false;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
